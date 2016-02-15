6b306aeba7204f0b43419b6cc29c1683969da9fc
Bez Hermoso <bezalelhermoso@gmail.com>
2016-02-15 13:24:28 -0800

    Bugfix (nil pointer exception)

--- hammerspoon/modules/screen.lua
+++ hammerspoon/modules/screen.lua
@@ -3,6 +3,7 @@ local screen = require "hs.screen"
 local hotkey = require "hs.hotkey"
 local fnutils = require "hs.fnutils"
 local geometry = require "hs.geometry"
+local mouse = require "hs.mouse"
 require "hs.application"
 
 -- DISPLAY FOCUS SWITCHING --
@@ -17,7 +18,12 @@ local function isInScreen(sc, win)
   return win:screen() == sc
 end
 
-local function focusScreen(sc)
+local function moveMouseToScreen(sc)
+  local pt = geometry.rectMidPoint(sc:fullFrame())
+  mouse.setAbsolutePosition(pt)
+end
+
+local function focusScreen(sc, moveMouse)
   --Get windows within screen, ordered from front to back.
   --If no windows exist, bring focus to desktop. Otherwise, set focus on
   --front-most application window.
@@ -28,16 +34,22 @@ local function focusScreen(sc)
     fnutils.partial(isInScreen, sc))
   local windowToFocus = #windows > 0 and windows[1] or window.desktop()
   windowToFocus:focus()
+
+  if moveMouse then moveMouseToScreen(sc) end
 end
 
 --Bring focus to next display/screen
 hotkey.bind({"alt"}, "`", function ()
-  focusScreen(window.focusedWindow():screen():next())
+  local sc = window.focusedWindow():screen()
+  if not sc then return end
+  focusScreen(window.focusedWindow():screen():next(), true)
 end)
 
 --Bring focus to previous display/screen
 hotkey.bind({"alt", "shift"}, "`", function()
-  focusScreen(window.focusedWindow():screen():previous())
+  local sc = window.focusedWindow():screen()
+  if not sc then return end
+  focusScreen(window.focusedWindow():screen():previous(), true)
 end)
 
 -- END DISPLAY FOCUS SWITCHING --
