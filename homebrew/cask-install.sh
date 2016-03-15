aa72d5606f2fd096adf714ff6a3aedb9bd6efe67
Bez Hermoso <bezalelhermoso@gmail.com>
2016-03-14 21:55:53 -0700

    Homebrew

---
+++ homebrew/cask-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+brew install amethyst
+brew install beardedspice
+brew install colorpicker-skalacolor
+brew install cyberduck
+brew install firefox
+brew install gyazo
+brew install heroku-toolbelt
+brew install macdown
+brew install postbox
+brew install rightfont
+brew install thyme
+brew install transmission
+brew install unsplash-wallpaper
+brew install vagrant
+brew install virtualbox
