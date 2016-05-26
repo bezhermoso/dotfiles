753cdc7209e330eb1b4a520cb54f1a806ee86598
Bez Hermoso <bezalelhermoso@gmail.com>
2016-05-25 23:49:58 -0700

    Subdued green

--- tmux/status-left.zsh
+++ tmux/status-left.zsh
@@ -3,13 +3,13 @@
 tmux_session_colors=(white colour8)
 tmux_session_name=" #{session_name} "
 
-tmux_session_prefix_colors=(colour42 colour3)
+tmux_session_prefix_colors=(colour29 colour29)
 
-tmux_session_segment="#[fg=$tmux_session_prefix_colors[1]]#{?client_prefix,,$tmux_session_name}"
-tmux_session_segment="$tmux_session_segment#{?client_prefix,,❯❯}"
+tmux_session_segment="#[fg=$tmux_session_prefix_colors[1],bold]#{?client_prefix,,($tmux_session_name)}"
+tmux_session_segment="$tmux_session_segment#{?client_prefix,,}"
 
-tmux_session_prefix_segment="#[fg=$tmux_session_prefix_colors[2]]#{?client_prefix,$tmux_session_name,}"
-tmux_session_prefix_segment="$tmux_session_prefix_segment#{?client_prefix,❯❯,}"
+tmux_session_prefix_segment="#[bg=$tmux_session_prefix_colors[2],fg=colour232,bold]#{?client_prefix, $tmux_session_name ,}"
+tmux_session_prefix_segment="$tmux_session_prefix_segment#{?client_prefix,,}"
 
 tmux_status_left="$tmux_session_segment$tmux_session_prefix_segment#[default]  "
 
--- tmux/status-right.zsh
+++ tmux/status-right.zsh
@@ -2,35 +2,36 @@
 
 source "$HOME/.tmux-statuslinerc"
 
-ruby_colors=(colour52 colour1)
+sep="❮"
+ruby_colors=(colour52 colour124)
 ruby_version=$($TMUX_CONFIG_DIR/get-tmux-env.zsh TMUX_RUBY_V)
 ruby_lower_bound=120
 ruby_version="${ruby_version:----}"
 if [[ -n $ruby_version ]]; then
-  ruby_segment="#[fg=$ruby_colors[2]] ❮ RB $ruby_version#[default]"
+  ruby_segment="#[fg=$ruby_colors[2]] rb=$ruby_version /#[default]"
 else
   ruby_segment=
 fi
 
-node_colors=(colour22 colour2)
+node_colors=(colour22 colour29)
 node_version="$($TMUX_CONFIG_DIR/get-tmux-env.zsh TMUX_NODEJS_V)"
 node_lower_bound=120
 node_version="${node_version:----}"
 if [[ -n $node_version ]]; then
-  node_segment="#[fg=$node_colors[2]] ❮ JS $node_version#[default]"
+  node_segment="#[fg=$node_colors[2]] js=$node_version /#[default]"
 else
   node_segment=
 fi
 
 
-python_colors=(colour214 colour227)
+python_colors=(colour208 colour227)
 python_fg=black
 python_version=$($TMUX_CONFIG_DIR/get-tmux-env.zsh TMUX_PYTHON_V)
 python_lower_bound=120
 python_version="${python_version:----}"
 
 if [[ -n $python_version ]]; then
-  python_segment="#[fg=$python_colors[1]] ❮ PY $python_version#[default]"
+  python_segment="#[fg=$python_colors[1]] py=$python_version /#[default]"
 else
   python_segment=
 fi
@@ -41,7 +42,7 @@ php_lower_bound=120
 php_version=$($TMUX_CONFIG_DIR/get-tmux-env.zsh TMUX_PHP_V)
 php_version="${php_version:----}"
 if [[ -n $python_version ]]; then
-  php_segment="#[fg=$php_colors[2]] ❮ PHP $php_version#[default]"
+  php_segment="#[fg=$php_colors[1]] php=$php_version /#[default]"
 else
   php_segment=
 fi
@@ -49,7 +50,8 @@ fi
 tmux_time_colors=(colour24 colour29 colour42)
 tmux_time_format="%H:%M"
 tmux_date_format="%b %d"
-time_segment="#[fg=$tmux_time_colors[3]] ❮ $(date +$tmux_date_format) #[bg=$tmux_time_colors[2],fg=white] $(date +$tmux_time_format) "
+
+time_segment="#[fg=$tmux_time_colors[3]]  $(date +$tmux_date_format) /#[fg=$tmux_time_colors[3]] $(date +$tmux_time_format) "
 
 $power_segment=""
 #local battery_level
@@ -102,6 +104,6 @@ if [[ $window_width -gt $php_lower_bound ]]; then
 fi
 
 
-tmux_status_right="$tmux_status_right   $power_segment$time_segment"
+tmux_status_right="$tmux_status_right$power_segment$time_segment"
 tmux display-message -p "$tmux_status_right"
 
