142f6440c6c2c75f189abbd02d511253cc43b4b4
Bez Hermoso <bezalelhermoso@gmail.com>
2016-08-11 12:38:14 -0700

    Improved tmux statuslines.

--- tmux/status-right.zsh
+++ tmux/status-right.zsh
@@ -2,13 +2,13 @@
 
 source "$HOME/.tmux-statuslinerc"
 
-sep="❮"
+sep=" <"
 ruby_colors=(colour52 colour1)
 ruby_version=$($TMUX_CONFIG_DIR/get-tmux-env.zsh TMUX_RUBY_V)
 ruby_lower_bound=120
 ruby_version="${ruby_version:----}"
 if [[ -n $ruby_version ]]; then
-  ruby_segment="#[fg=$ruby_colors[2]]〈 rb=$ruby_version#[default]"
+  ruby_segment="#[fg=$ruby_colors[2]]$sep rb=$ruby_version#[default]"
 else
   ruby_segment=
 fi
@@ -18,7 +18,7 @@ node_version="$($TMUX_CONFIG_DIR/get-tmux-env.zsh TMUX_NODEJS_V)"
 node_lower_bound=120
 node_version="${node_version:----}"
 if [[ -n $node_version ]]; then
-  node_segment="#[fg=$node_colors[2]]〈 js=$node_version#[default]"
+  node_segment="#[fg=$node_colors[2]]$sep js=$node_version#[default]"
 else
   node_segment=
 fi
@@ -31,7 +31,7 @@ python_lower_bound=120
 python_version="${python_version:----}"
 
 if [[ -n $python_version ]]; then
-  python_segment="#[fg=$python_colors[1]]〈 py=$python_version#[default]"
+  python_segment="#[fg=$python_colors[1]]$sep py=$python_version#[default]"
 else
   python_segment=
 fi
@@ -42,7 +42,7 @@ php_lower_bound=120
 php_version=$($TMUX_CONFIG_DIR/get-tmux-env.zsh TMUX_PHP_V)
 php_version="${php_version:----}"
 if [[ -n $python_version ]]; then
-  php_segment="#[fg=$php_colors[1]]〈 php=$php_version#[default]"
+  php_segment="#[fg=$php_colors[1]]$sep php=$php_version#[default]"
 else
   php_segment=
 fi
@@ -51,7 +51,7 @@ tmux_time_colors=(colour24 colour29 colour42)
 tmux_time_format="%H:%M"
 tmux_date_format="%b %d"
 
-time_segment="#[fg=$tmux_time_colors[3]] ● $(date +$tmux_date_format) #[fg=$tmux_time_colors[3]]$(date +$tmux_time_format) "
+time_segment="#[fg=$tmux_time_colors[3]] ∷ $(date +$tmux_date_format) #[fg=$tmux_time_colors[3]]$(date +$tmux_time_format) "
 
 $power_segment=""
 #local battery_level
@@ -90,16 +90,16 @@ local tmux_status_right=""
 
 tmux_status_right=""
 
-if [[ $window_width -gt $node_lower_bound ]]; then
+if [[ -n $node_segment && $window_width -gt $node_lower_bound ]]; then
   tmux_status_right="$tmux_status_right$node_segment"
 fi
-if [[ $window_width -gt $ruby_lower_bound ]]; then
+if [[ -n $ruby_segment && $window_width -gt $ruby_lower_bound ]]; then
   tmux_status_right="$tmux_status_right$ruby_segment"
 fi
-if [[ $window_width -gt $python_lower_bound ]]; then
+if [[ -n $python_segment && $window_width -gt $python_lower_bound ]]; then
   tmux_status_right="$tmux_status_right$python_segment"
 fi
-if [[ $window_width -gt $php_lower_bound ]]; then
+if [[ -n $php_segment && $window_width -gt $php_lower_bound ]]; then
   tmux_status_right="$tmux_status_right$php_segment"
 fi
 
