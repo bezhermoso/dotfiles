19f60d70f0fe2a481a3ea7671c0c676c95730019
Bez Hermoso <bezalelhermoso@gmail.com>
2017-10-28 01:27:43 -0700

    Fix indentation

--- nodejs/inventory.sh
+++ nodejs/inventory.sh
@@ -10,14 +10,14 @@ function _npm-inventory() {
 
 function _npm-install-version() {
   local version=$1
-    #echo '# version '$v
-    if [[ "$version" != "system"  ]]; then
-      echo "echo \"Installing $version...\""
-      echo nodenv install $version --skip-existing
-    fi
-    echo "echo \"Installing dependencies for $version...\""
-    echo "NODENV_VERSION=$version npm install -g \\"
-    NODENV_VERSION=$version npm ls --depth=0 --global 2>/dev/null | awk 'NR>1 { print "  " $NF }'
+  #echo '# version '$v
+  if [[ "$version" != "system" ]]; then
+    echo "echo \"Installing $version...\""
+    echo nodenv install $version --skip-existing
+  fi
+  echo "echo \"Installing dependencies for $version...\""
+  echo "NODENV_VERSION=$version npm install -g \\"
+  NODENV_VERSION=$version npm ls --depth=0 --global 2>/dev/null | awk 'NR>1 { print "  " $NF }'
 }
 
 _npm-inventory
