084250fec3ff85b1c8b2ef35fd3ad04efc6c830e
Bez Hermoso <bezalel@squareup.com>
2023-07-17 18:23:43 -0700

    Use base16 neovim themes, with gruvbox-dark-hard

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,19 @@
 
 return {
     {
-        "nvim-tree/nvim-web-devicons",
+        'nvim-tree/nvim-web-devicons',
         lazy = true,
+    },
+    {
+        'ellisonleao/gruvbox.nvim',
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme base16-gruvbox-dark-hard]])
+        end
+    },
+    {
+        'RRethy/nvim-base16',
+        priority = 1000,
     },
     {
         "catppuccin/nvim",
@@ -10,7 +21,7 @@ return {
         priority = 1000,
         lazy = false,
         config = function()
-            vim.cmd([[colorscheme catppuccin]])
+          --  vim.cmd([[colorscheme catppuccin]])
         end
     }
 }
