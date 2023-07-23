689985e398743847f717bfadfe7d846fa7a204df
Bez Hermoso <bezalel@squareup.com>
2023-07-22 17:17:06 -0700

    base16: Switch to tinted-theming/base16-shell

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -22,11 +22,14 @@ return {
         'RRethy/nvim-base16',
         priority = 1000,
         config = function()
-            local base16_adopt_shell_theme = function ()
-            local themepath = vim.fn.expand('~/.vimrc_background')
-                if vim.loop.fs_stat(themepath) then
-                    print('Loading theme from ' .. themepath)
-                    vim.cmd('source ' .. themepath)
+            local base16_adopt_shell_theme = function()
+                local fn = vim.fn
+                local cmd = vim.cmd
+                local set_theme_path = "$HOME/.config/tinted-theming/set_theme.lua"
+                local is_set_theme_file_readable = fn.filereadable(fn.expand(set_theme_path)) == 1 and true or false
+                if is_set_theme_file_readable then
+                    cmd("let base16colorspace=256")
+                    cmd("source " .. set_theme_path)
                 end
             end
             base16_adopt_shell_theme()
@@ -44,4 +47,3 @@ return {
         end
     },
 }
-
