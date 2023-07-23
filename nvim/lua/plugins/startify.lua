ab529178c8de01bd4c86b0ef3f75309cef179a46
Bez Hermoso <bezalelhermoso@gmail.com>
2023-07-22 22:44:51 -0700

    startify: Use "dashboard" theme

--- nvim/lua/plugins/startify.lua
+++ nvim/lua/plugins/startify.lua
@@ -2,13 +2,14 @@ return {
     'goolord/alpha-nvim',
     requires = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-         local alpha = require('alpha')
-         local startify = require('alpha.themes.startify')
-         local handle = io.popen('fortune -s')
-         local fortune = handle:read("*a")
-         handle:close()
-         startify.section.header.val = fortune
-         alpha.setup(startify.config)
+        local alpha = require('alpha')
+        local startify = require('alpha.themes.dashboard')
+        local handle = io.popen('fortune -s')
+        if handle ~= nil then
+            local fortune = handle:read("*a")
+            handle:close()
+            startify.section.header.val = fortune
+        end
+        alpha.setup(startify.config)
     end
 }
-
