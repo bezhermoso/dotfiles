8fdc2ec26a661670b35446203ba391cfbe861af5
Bez Hermoso <bezalel@squareup.com>
2023-07-24 12:57:34 -0700

    nvim: lazy.nvim: Move to lua/core/lazy.lua

---
+++ nvim/lua/core/lazy.lua
@@ -0,0 +1,21 @@
+--Bootstrap lazy.nvim plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = 'plugins',
+    dev = {
+        path = '~/Development',
+    },
+})
+
--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,30 +1,7 @@
 require("core.options")
 
--- TODO: Disable treesitter highlights to work with catppuccin: https://github.com/catppuccin/nvim#wrong-treesitter-highlights
-
---Bootstrap lazy.nvim plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-    spec = 'plugins',
-    dev = {
-        path = '~/Development',
-    },
-})
+require("core.lazy")
 
 require("core.backup")
-require("core.undo")
 require("core.swap")
-
 require('core.keymaps')
