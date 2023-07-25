7f64e3456a3d7e46ac3c593499318189c8561aaf
Bez Hermoso <bezalel@squareup.com>
2023-07-25 16:39:06 -0700

    feat: nvim: More LSP config & bindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -14,31 +14,28 @@ return {
             'debugloop/telescope-undo.nvim',
         },
         keys = {
+            { "<leader>*",   ":lua require('telescope.builtin').grep_string()<CR>",     desc = 'Telescope: Grep string...', mode = { 'v', 'n' } },
             { "<leader>F",   ":lua require('telescope.builtin').resume()<CR>",          desc = 'Telescope: Resume' },
-            { "<leader>*",   ":lua require('telescope.builtin').grep_string()<CR>",     desc =
-            'Telescope: Grep string...',                                                                                         mode = {
-                'v', 'n' } },
-            { "<leader>fQ",  ":lua require('telescope.builtin').quickfixhistory()<CR>",
-                                                                                            desc =
-                'Telescope: Quickfix (History)' },
+            { "<leader>fGb", ":lua require('telescope.builtin').git_branch()<CR>",      desc = 'Telescope: Git branches' },
+            { "<leader>fGs", ":lua require('telescope.builtin').git_status()<CR>",      desc = 'Telescope: Git status' },
+            { "<leader>fO",  ":lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<CR>",    desc = 'Telescope: LSP Symbols (Dynamic)' },
+            { "<leader>fQ",  ":lua require('telescope.builtin').quickfixhistory()<CR>", desc = 'Telescope: Quickfix (History)' },
+            { "<leader>fa",  ":lua require('telescope.builtin').find_files({ no_ignore = true })<CR>", desc = 'Telescope: Files (All)' },
             { "<leader>fb",  ":lua require('telescope.builtin').buffers()<CR>",         desc = 'Telescope: Buffers' },
             { "<leader>fc",  ":lua require('telescope.builtin').colorscheme()<CR>",     desc = 'Telescope: Colorschemes' },
             { "<leader>ff",  ":lua require('telescope.builtin').find_files()<CR>",      desc = 'Telescope: Files' },
             { "<leader>fg",  ":lua require('telescope.builtin').live_grep()<CR>",       desc = 'Telescope: Live Grep' },
             { "<leader>fh",  ":lua require('telescope.builtin').help_tags()<CR>",       desc = 'Telescope: Help tags' },
             { "<leader>fm",  ":lua require('telescope.builtin').man_pages()<CR>",       desc = 'Telescope: man pages' },
+            { "<leader>fo",  ":lua require('telescope.builtin').lsp_workspace_symbols()<CR>",    desc = 'Telescope: LSP Symbols' },
             { "<leader>fq",  ":lua require('telescope.builtin').quickfix()<CR>",        desc = 'Telescope: Quickfix' },
             { "<leader>fr",  ":lua require('telescope.builtin').oldfiles()<CR>",        desc = 'Telescope: MRU' },
-            { "<leader>fs",  ":lua require('telescope.builtin').spell_suggest()<CR>",
-                                                                                            desc =
-                'Telescope: Spelling Suggestions' },
-            { "<leader>fy",  ":lua require('telescope.builtin').registers()<CR>",       desc = 'Telescope: Registers' },
+            { "<leader>fs",  ":lua require('telescope.builtin').spell_suggest()<CR>", desc = 'Telescope: Spelling Suggestions' },
             { "<leader>ft",  ":lua require('telescope.builtin').builtin()<CR>",         desc = 'Telescope: Built-in' },
-            { "<leader>fGs", ":lua require('telescope.builtin').git_status()<CR>",      desc = 'Telescope: Git status' },
-            { "<leader>fGb", ":lua require('telescope.builtin').git_branch()<CR>",      desc = 'Telescope: Git branches' },
             { "<leader>fu",  ":lua require('telescope').extensions.undo.undo()<CR>",    desc = 'Telescope: Undo' },
+            { "<leader>fy",  ":lua require('telescope.builtin').registers()<CR>",       desc = 'Telescope: Registers' },
         },
-        config = function(plugin, opts)
+        config = function(_, opts)
             local telescope = require('telescope')
             telescope.setup(opts)
             telescope.load_extension('fzf')
