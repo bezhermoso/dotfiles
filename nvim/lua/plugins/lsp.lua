82df5d3c936b23de0311bf0e2d63c34fa0709983
Bez Hermoso <bezalel@squareup.com>
2023-07-25 09:33:14 -0700

    nvim: Use lsp-zero + consolidate "lsp" and "completion" plugin groups into "lsp"

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,26 +1,52 @@
 return {
     {
-        'williamboman/mason.nvim',
-        build = ':MasonInstall',
-        lazy = true,
-    },
-    {
-        'williamboman/mason-lspconfig.nvim',
-        lazy = true,
-    },
-    {
-        'neovim/nvim-lspconfig',
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
         dependencies = {
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
-
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            {
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.api.nvim_command, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-cmdline' },
+            { 'hrsh7th/cmp-path' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-calc' },
+            { 'f3fora/cmp-spell' },
+            { 'tamago324/cmp-zsh' },
+            { 'quangnguyen30192/cmp-nvim-ultisnips' },
+            { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+            { 'hrsh7th/cmp-emoji' },
+            { 'andersevenrud/cmp-tmux' },
+            { 'KadoBOT/cmp-plugins' },
+            { 'dmitmel/cmp-cmdline-history' },
+            { 'chrisgrieser/cmp-nerdfont' },
         },
         config = function()
+            -- Language Server Protocol (LSP) {{{
             require('mason').setup()
             require('mason-lspconfig').setup({
-                ensure_installed = {'gopls', 'phpactor', 'lua_ls', 'intelephense'},
+                ensure_installed = {
+                    'gopls',        -- Go
+                    'intelephense', -- PHP
+                    'lua_ls',       -- Lua
+                    'phpactor',     -- PHP
+                },
             })
-            local on_attach = function ()
+            local lsp_zero = require('lsp-zero').preset({})
+            lsp_zero.on_attach(function(_, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp_zero.default_keymaps({ buffer = bufnr })
                 vim.keymap.set('n', '<leader>mv', vim.lsp.buf.rename, {
                     desc = 'LSP: Rename',
                 })
@@ -42,54 +68,45 @@ return {
                 vim.keymap.set('n', '<leader>==', vim.lsp.buf.format, {
                     desc = 'LSP: Format',
                 })
-            end
-            local lspconfig = require('lspconfig')
-            lspconfig.gopls.setup({
-                on_attach = on_attach,
+            end)
+            -- Make Lua language server understand Neovim API
+            require('lspconfig').lua_ls.setup(lsp_zero.nvim_lua_ls())
+            lsp_zero.setup({
+                set_extra_mappings = true,
             })
-            lspconfig.phpactor.setup({
-                on_attach = on_attach,
-            })
-            lspconfig.lua_ls.setup({
-                on_attach = on_attach,
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = {'vim'},
-                        }
-                    }
+            -- }}}
+
+            -- COMPLETION {{{
+            -- lsp-zero already sets up nvim-cmp for us, here we are simply
+            -- setting more configuration e.g. adding sources.
+            local cmp = require('cmp')
+            cmp.setup({
+                sources = {
+                    { name = 'nvim_lsp', priority = 1000 },
+                    { name = 'luasnip', priority = 750 },
+                    { name = 'buffer', priority = 500 },
+                    { name = 'path', priority = 250 },
+                    { name = 'nvm_lsp_signature_help' },
+                    { name = 'calc' },
+                    { name = 'spell' },
+                    { name = 'zsh' },
+                    { name = 'tmux' },
+                    { name = 'emoji' },
+                    { name = 'plugins' },
+                    { name = 'nerdfont' },
+                },
+                mapping = {
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 }
             })
-            lspconfig.intelephense.setup({
-                on_attach = on_attach,
-            })
+            -- }}}
         end
     },
-    --    {
-    --        "ms-jpq/coq_nvim",
-    --        branch = "coq",
-    --        dependencies = {
-    --            {
-    --                "ms-jpq/coq.artifacts",
-    --                branch = "artifacts",
-    --            },
-    --            {
-    --                "neovim/nvim-lspconfig",
-    --            }
-    --            --{
-    --            --    "ms-jpq/coc.thirdparty",
-    --            --    branch = "3p"
-    --            --}
-    --        },
-    --        lazy = false,
-    --        build = ":COQdeps",
-    --        init = function()
-    --            vim.g.coq_settings = {
-    --                auto_start = true
-    --            }
-    --        end,
-    --        config = function()
-    --            require('coq')
-    --        end
-    --    }
+    {
+        'numToStr/Comment.nvim',
+        event = 'InsertEnter',
+        config = function()
+            require('Comment').setup()
+        end
+    },
 }
