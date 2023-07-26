db21849a98368c00035980844ca009b50458d669
Bez Hermoso <bezalel@squareup.com>
2023-07-25 18:56:00 -0700

    feat: nvim: <leader>U for Undotree

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,7 +1,7 @@
 return {
     "mbbill/undotree",
     keys = {
-        {"<leader>u", vim.cmd.UndotreeToggle, desc = "UndoTree: Toggle"},
+        {"<leader>U", vim.cmd.UndotreeToggle, desc = "UndoTree: Toggle"},
     },
     init = function()
         -- Enable persistent undo
