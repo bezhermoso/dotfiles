c5f0e2448de081214136919439ff14158c2f6efd
Bez Hermoso <bezalelhermoso@gmail.com>
2023-07-30 22:32:10 -0700

    wip: open plugin in Github keymap

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -2,3 +2,18 @@
 vim.keymap.set('n', '<leader><Space>', ':noh<CR>', {
     desc = 'Clear buffer search'
 })
+
+local open_plugin_github_repo = function ()
+    local plugin = vim.fn.expand('<cWORD>')
+    if plugin == nil then
+        return
+    end
+    plugin = string.gsub(plugin, '"', '')
+    plugin = string.gsub(plugin, '\'', '')
+    local cmd = '!open https://github.com/' .. plugin
+    vim.cmd(cmd)
+end
+
+vim.keymap.set('n', '<leader>gh', open_plugin_github_repo)
+
+-- "test/repo"
