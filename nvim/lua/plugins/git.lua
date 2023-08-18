4c5ea0a94586492c5e505d976597c7340e529d50
Bez Hermoso <bezalel@squareup.com>
2023-08-18 12:21:37 -0700

    nvim: Add openingh.nvim

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -11,5 +11,19 @@ return {
         enabled = vim.fn.executable('git') == 1,
         lazy = false,
         opts = {}
+    },
+    {
+        "almo7aya/openingh.nvim",
+        enabled = vim.fn.executable('gh') == 1,
+        lazy = false,
+        config = function()
+            require('openingh').setup()
+            -- for repository page
+            vim.api.nvim_set_keymap("n", "<Leader>gr", ":OpenInGHRepo <CR>", { silent = true, noremap = true, desc = 'Open repo in Github' })
+
+            -- for current file page
+            vim.api.nvim_set_keymap("n", "<Leader>gf", ":OpenInGHFile <CR>", { silent = true, noremap = true, desc = 'Open in Github' })
+            vim.api.nvim_set_keymap("v", "<Leader>gf", ":OpenInGHFileLines <CR>", { silent = true, noremap = true, desc = 'Open it Github' })
+        end
     }
 }
