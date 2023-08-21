9dd67edfa007f7cad43002e98b578ff77371510b
Bez Hermoso <bezalel@squareup.com>
2023-08-21 12:29:08 -0700

    nvim: dressing: center the popup input widget

--- nvim/lua/plugins/dressing.lua
+++ nvim/lua/plugins/dressing.lua
@@ -1,10 +1,17 @@
 return {
     'stevearc/dressing.nvim',
     opts = {
-        insert_only = false,
         select = {
             enable = false,
             backend = {'telescope'}
+        },
+        input = {
+            insert_only = false,
+            start_in_insert = false,
+            relative = "editor"
+        },
+        win_options = {
+            sidescrolloff = 10,
         }
     },
 }
