065c57025d623f7aaab5f1b6bdf25f6bf622b29d
Bez Hermoso <bezalelhermoso@gmail.com>
2023-11-16 23:42:23 -0800

    feat: nvim: Configure vim-marked better

--- nvim/lua/plugins/vim-marked.lua
+++ nvim/lua/plugins/vim-marked.lua
@@ -1,3 +1,7 @@
 return {
     'itspriddle/vim-marked',
+    ft = {'markdown', 'mkd', 'ghmarkdown', 'vimwiki'},
+    keys = {
+        {"<leader>M", ":MarkedToggle!<CR>", desc = 'Markdown: Open in Marked', silent = true}
+    }
 }
