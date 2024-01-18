74355973cb8840b9f60d07448f87138e4568744e
Bez Hermoso <bezalelhermoso@gmail.com>
2024-01-17 22:06:48 -0800

    feat: nvim: Parse *.tfvars as Terraform files

--- nvim/lua/core/filetypes.lua
+++ nvim/lua/core/filetypes.lua
@@ -8,12 +8,24 @@
 --   }
 -- })
 
+-- YAML
 vim.filetype.add({
   extension = {
     neon = "yaml",
     ["yaml.template"] = "gotexttmpl.yaml"
   },
+})
+
+-- Ruby
+vim.filetype.add({
   filename = {
     Brewfile = "ruby"
   }
+})
+
+-- Terraform
+vim.filetype.add({
+  extension = {
+    tfvars = "terraform",
+  },
 })
