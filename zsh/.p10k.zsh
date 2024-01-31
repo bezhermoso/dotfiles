945c6a50269723c4e54929a2178e7c2af000f2c6
Bez Hermoso <bezalelhermoso@gmail.com>
2024-01-30 18:33:46 -0800

    feat: p10k: Show "lock" icon for read-only or missing cwd

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -292,7 +292,7 @@
 
   # The default icon shown next to non-writable and non-existent directories when
   # POWERLEVEL9K_DIR_SHOW_WRITABLE is set to v3.
-  # typeset -g POWERLEVEL9K_LOCK_ICON='⭐'
+  typeset -g POWERLEVEL9K_LOCK_ICON=''
 
   # POWERLEVEL9K_DIR_CLASSES allows you to specify custom icons and colors for different
   # directories. It must be an array with 3 * N elements. Each triplet consists of:
