b90d414c2b631897e6d7a92eda4e09edad0ee81f
Bez Hermoso <bezalelhermoso@gmail.com>
2024-01-30 23:22:09 -0800

    feat: nvim: Install & setup Spectre

---
+++ nvim/lua/plugins/spectre.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        "https://github.com/nvim-pack/nvim-spectre",
+        cmd = {"Spectre"},
+        keys = {
+            { "<leader>S", function() require("spectre").toggle() end, {
+                desc = "Spectre: Toggle",
+            } },
+            { "<leader>sw", function() require("spectre").open_visual({ select_word = true }) end, {
+                desc = "Spectre: Open w/ word under cursor",
+            } },
+            { "<leader>sw", function() require("spectre").open_visual() end, {
+                desc = "Spectre: Search word under cursor",
+                mode = "v"
+            } },
+            { "<leader>sf", function() require("spectre").open_file_search({ select_word = true }) end, {
+                desc = "Spectre: Search word in file",
+                mode = "v"
+            } },
+        }
+    }
+}
