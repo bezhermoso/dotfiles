8ba3e6aa3e74db3c7800a33623933b658912ece3
Bez Hermoso <bezalelhermoso@gmail.com>
2024-02-09 20:10:28 -0800

    feat: nvim: nvim-ufo: use treesitter instead of lsp as provider for folding for some filetypes

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -1,4 +1,13 @@
 
+-- Apparently there are filetypes whose LSP does not support folding. We can fallback to Treesitter instead.
+-- Source: https://github.com/chrisgrieser/.config/blob/a3c5be040b46f7dc43e5e10c15c807fa505bcb10/nvim/lua/plugins/folding-plugins.lua#L65-L72
+local treesitter_based_folding_filetypes = {
+    "markdown",
+    "sh",
+    "css",
+    "html",
+    "python",
+}
 return {
     "https://github.com/kevinhwang91/nvim-ufo",
     lazy = false,
@@ -19,6 +28,9 @@ return {
     config = function()
         require('ufo').setup({
             provider_selector = function (_, ftype, _)
+                if vim.tbl_contains(treesitter_based_folding_filetypes, ftype) then
+                    return {'treesitter', 'indent'}
+                end
                 return {'lsp', 'indent'}
             end
         })
