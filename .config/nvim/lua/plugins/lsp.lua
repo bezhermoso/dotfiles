c5708847c7835c3f2b8e958028d7e39178a9ec18
Bez Hermoso <bezalelhermoso@gmail.com>
2024-02-15 21:24:58 -0800

    feat: nvim: luasnip: load friendly-snippets to LuaSnip corpus

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -351,10 +351,15 @@ return {
     },
     {
         "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
         -- follow latest release.
         version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
         -- install jsregexp (optional!).
-        build = "make install_jsregexp"
+        build = "make install_jsregexp",
+        config = function()
+            require("luasnip").setup()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end
     },
     -- {
     --     'zbirenbaum/copilot.lua',
