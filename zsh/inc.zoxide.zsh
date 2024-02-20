368d9687bded1c7127ac31c243ae1199a977718e
Bez Hermoso <bezalelhermoso@gmail.com>
2024-02-20 01:04:52 -0800

    feat: zsh: Bind zoxide C-u and C-h to popd

--- zsh/inc.zoxide.zsh
+++ zsh/inc.zoxide.zsh
@@ -10,10 +10,25 @@ function _zoxide-cd-widget {
   zle push-line
   __zoxide_zi
   zle accept-line
-  zle reset-prompt
 }
+
+# ZLE widget for pulling up interactive zoxide & change directory.
+function _popd-widget {
+  zle push-line
+  popd 2>/dev/null
+  if [[ "$?" -gt 0 ]]; then
+      echo "\ndirstack is empty!"
+  fi
+  zle accept-line
+}
+
 
 # Bind interactive zoxide widget to Ctrl-H
 zle -N _zoxide-cd-widget
-bindkey -M vicmd '^h' _zoxide-cd-widget
-bindkey -M viins '^h' _zoxide-cd-widget
+zle -N _popd-widget
+bindkey -M vicmd '^u' _zoxide-cd-widget
+bindkey -M viins '^u' _zoxide-cd-widget
+
+bindkey -M vicmd '^h' _popd-widget
+bindkey -M viins '^h' _popd-widget
+
