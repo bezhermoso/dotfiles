060046478561593614200196a9968c4ef898f2e7
Bez Hermoso <bezalelhermoso@gmail.com>
2024-02-21 00:20:14 -0800

    feat: grep: more GREP_COLORS experiments
    Black on yello background for matches. Dimmed & italicized surrounding context.

--- zsh/inc.grep.zsh
+++ zsh/inc.grep.zsh
@@ -3,7 +3,8 @@ export GREP_COLOR='103;30'
 if (( $+commands[ggrep] )); then
   # BSD grep, the one that comes w/ macOS does not respect GREP_COLORS, only GREP_COLOR
   # Only configure GREP_COLORS when GNU grep is present, unset GREP_COLOR, then alias grep to ggrep
-  export GREP_COLORS="ms=${GREP_COLOR}"
+  # See https://askubuntu.com/a/1042242 for a good breakdown what these integers mean
+  export GREP_COLORS="ms=${GREP_COLOR}:cx=2;3"
   unset GREP_COLOR
   alias grep='nocorrect ggrep --color=auto'
 fi
