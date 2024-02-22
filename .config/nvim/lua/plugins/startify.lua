c2b001b9f06336e5ebf68cbd3101d59aea16f539
Bez Hermoso <bezalelhermoso@gmail.com>
2024-02-21 22:51:57 -0800

    feat: nvim: swith to theta theme

--- .config/nvim/lua/plugins/startify.lua
+++ .config/nvim/lua/plugins/startify.lua
@@ -6,13 +6,15 @@ return {
         local dashboard = require('alpha.themes.dashboard')
         local handle = io.popen('fortune -s')
         local startify = require("plugins.alpha.startify-centered").config
+        local theta = require('alpha.themes.theta')
         if handle ~= nil then
             local fortune = handle:read("*a")
             handle:close()
             dashboard.section.header.val = fortune
             startify.layout[2].val = vim.split(fortune, "\n")
+            theta.header.val = fortune
         end
-        alpha.setup(startify)
+        alpha.setup(theta.config)
     end
 }
 
