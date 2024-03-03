33130c02c40639816d681eed5990e1418378bb45
Bez Hermoso <bezalelhermoso@gmail.com>
2024-03-03 14:09:34 -0800

    feat: nvim: Install & configure bufferline.nvim

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "https://github.com/akinsho/bufferline.nvim",
+        dependenciesa = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        lazy = false,
+        keys = {
+            {"]b", ":BufferLineCycleNext<CR>", { desc = "Go to next buffer", mode = {'v', 'n'} }},
+            {"[b", ":BufferLineCyclePrevious<CR>", { desc = "Go to previous buffer", mode = {'v', 'n'} }},
+        },
+        config = function ()
+            local bufferline = require("bufferline")
+            bufferline.setup({
+                options = {
+                    style_preset = bufferline.style_preset.minimal,
+                    diagnostics = "nvim_lsp",
+                    always_show_bufferline = false,
+                }
+            })
+        end
+    }
+}
