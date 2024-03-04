a887fd239c1c3a7bf65fe4983689fc7d844717f9
Bez Hermoso <bezalelhermoso@gmail.com>
2024-03-03 23:37:13 -0800

    feat: git: switch to signing with gpg when SSHed into machine e.g. from iPad

--- zsh/inc.git.zsh
+++ zsh/inc.git.zsh
@@ -5,3 +5,13 @@ export LG_CONFIG_FILE=$HOME/.config/lazygit/config.yml
 unalias gr
 alias gr='jump-to-git-root'
 alias lg='lazygit'
+
+# Git configuration for when we are SSHed into the machine.
+# 1.) We configure Git to read config values from env via --config-env=<config_name>=<env_name>
+# 2.) We set the various env vars to certain values
+# 3.) Alias git to add the --config-env options.
+if [[ -n "$SSH_CONNECTION" ]]; then
+  export GIT_CONFIG_GPG_FORMAT="openpgp"
+  export GIT_CONFIG_USER_SIGNINGKEY="55BDEC097B885413"
+  alias git="git --config-env=user.signingkey=GIT_CONFIG_USER_SIGNINGKEY --config-env=gpg.format=GIT_CONFIG_GPG_FORMAT"
+fi
