bb23f56f2e6a99912cc925f6290fbb1250611e0f
Bez Hermoso <bezalelhermoso@gmail.com>
2024-03-08 08:52:55 -0800

    style: nvim: format telescope.lua

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -16,28 +16,28 @@ return {
             'gbprod/yanky.nvim',
         },
         keys = {
-            { "<leader>*",   ":lua require('telescope.builtin').grep_string()<CR>",     desc = 'Telescope: Grep string...', mode = { 'v', 'n' } },
-            { "<leader>F",   ":lua require('telescope.builtin').resume()<CR>",          desc = 'Telescope: Resume' },
-            { "<leader>fGb", ":lua require('telescope.builtin').git_branch()<CR>",      desc = 'Telescope: Git branches' },
-            { "<leader>fGs", ":lua require('telescope.builtin').git_status()<CR>",      desc = 'Telescope: Git status' },
-            { "<leader>fO",  ":lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<CR>",    desc = 'Telescope: LSP Symbols (Dynamic)' },
-            { "<leader>fQ",  ":lua require('telescope.builtin').quickfixhistory()<CR>", desc = 'Telescope: Quickfix (History)' },
+            { "<leader>*",   ":lua require('telescope.builtin').grep_string()<CR>",                    desc = 'Telescope: Grep string...',       mode = { 'v', 'n' } },
+            { "<leader>F",   ":lua require('telescope.builtin').resume()<CR>",                         desc = 'Telescope: Resume' },
+            { "<leader>fGb", ":lua require('telescope.builtin').git_branch()<CR>",                     desc = 'Telescope: Git branches' },
+            { "<leader>fGs", ":lua require('telescope.builtin').git_status()<CR>",                     desc = 'Telescope: Git status' },
+            { "<leader>fO",  ":lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<CR>",  desc = 'Telescope: LSP Symbols (Dynamic)' },
+            { "<leader>fQ",  ":lua require('telescope.builtin').quickfixhistory()<CR>",                desc = 'Telescope: Quickfix (History)' },
             { "<leader>fa",  ":lua require('telescope.builtin').find_files({ no_ignore = true })<CR>", desc = 'Telescope: Files (All)' },
-            { "<leader>fb",  ":lua require('telescope.builtin').buffers()<CR>",         desc = 'Telescope: Buffers' },
-            { "<leader>fc",  ":lua require('telescope.builtin').colorscheme()<CR>",     desc = 'Telescope: Colorschemes' },
-            { "<leader>ff",  ":lua require('telescope.builtin').find_files({ hidden = true })<CR>",      desc = 'Telescope: Files' },
-            { "<leader>fg",  ":lua require('telescope.builtin').live_grep()<CR>",       desc = 'Telescope: Live Grep' },
-            { "<leader>fh",  ":lua require('telescope.builtin').help_tags()<CR>",       desc = 'Telescope: Help tags' },
-            { "<leader>fm",  ":lua require('telescope.builtin').man_pages()<CR>",       desc = 'Telescope: man pages' },
-            { "<leader>fo",  ":lua require('telescope.builtin').lsp_workspace_symbols()<CR>",    desc = 'Telescope: LSP Symbols' },
-            { "<leader>fp",  ":lua require('telescope.builtin').builtin()<CR>",         desc = 'Telescope: Built-in' },
-            { "<leader>fq",  ":lua require('telescope.builtin').quickfix()<CR>",        desc = 'Telescope: Quickfix' },
-            { "<leader>fr",  ":lua require('telescope.builtin').oldfiles()<CR>",        desc = 'Telescope: MRU' },
-            { "<leader>fs",  ":lua require('telescope.builtin').spell_suggest()<CR>", desc = 'Telescope: Spelling Suggestions' },
-            { "<leader>ft",  ":lua require('telescope').extensions.file_browser.file_browser()<CR>",         desc = 'Telescope: File tree' },
-            { "<leader>fu",  ":lua require('telescope').extensions.undo.undo()<CR>",    desc = 'Telescope: Undo' },
-            { "<leader>fy",  ":lua require('telescope.builtin').registers()<CR>",       desc = 'Telescope: Registers' },
-            { "<leader>fY",  ":Telescope yank_history<CR>",       desc = 'Telescope: Yank history' },
+            { "<leader>fb",  ":lua require('telescope.builtin').buffers()<CR>",                        desc = 'Telescope: Buffers' },
+            { "<leader>fc",  ":lua require('telescope.builtin').colorscheme()<CR>",                    desc = 'Telescope: Colorschemes' },
+            { "<leader>ff",  ":lua require('telescope.builtin').find_files({ hidden = true })<CR>",    desc = 'Telescope: Files' },
+            { "<leader>fg",  ":lua require('telescope.builtin').live_grep()<CR>",                      desc = 'Telescope: Live Grep' },
+            { "<leader>fh",  ":lua require('telescope.builtin').help_tags()<CR>",                      desc = 'Telescope: Help tags' },
+            { "<leader>fm",  ":lua require('telescope.builtin').man_pages()<CR>",                      desc = 'Telescope: man pages' },
+            { "<leader>fo",  ":lua require('telescope.builtin').lsp_workspace_symbols()<CR>",          desc = 'Telescope: LSP Symbols' },
+            { "<leader>fp",  ":lua require('telescope.builtin').builtin()<CR>",                        desc = 'Telescope: Built-in' },
+            { "<leader>fq",  ":lua require('telescope.builtin').quickfix()<CR>",                       desc = 'Telescope: Quickfix' },
+            { "<leader>fr",  ":lua require('telescope.builtin').oldfiles()<CR>",                       desc = 'Telescope: MRU' },
+            { "<leader>fs",  ":lua require('telescope.builtin').spell_suggest()<CR>",                  desc = 'Telescope: Spelling Suggestions' },
+            { "<leader>ft",  ":lua require('telescope').extensions.file_browser.file_browser()<CR>",   desc = 'Telescope: File tree' },
+            { "<leader>fu",  ":lua require('telescope').extensions.undo.undo()<CR>",                   desc = 'Telescope: Undo' },
+            { "<leader>fy",  ":lua require('telescope.builtin').registers()<CR>",                      desc = 'Telescope: Registers' },
+            { "<leader>fY",  ":Telescope yank_history<CR>",                                            desc = 'Telescope: Yank history' },
         },
         config = function(_, opts)
             local telescope = require('telescope')
@@ -127,13 +127,13 @@ return {
             telescope.load_extension('file_browser')
             telescope.load_extension('yank_history')
 
-            vim.keymap.set('n', '<leader>/', function ()
+            vim.keymap.set('n', '<leader>/', function()
                 require('telescope.builtin').current_buffer_fuzzy_find(
                     require('telescope.themes').get_dropdown({
                         previewer = false,
                     })
                 )
-            end, { desc = "Find in current buffer..."})
+            end, { desc = "Find in current buffer..." })
         end,
     },
 }
