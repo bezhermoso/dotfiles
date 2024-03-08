08fc4c1b283ca9dec2dd3b45d053a973de8d51c7
Bez Hermoso <bezalelhermoso@gmail.com>
2024-03-08 00:27:24 -0800

    feat: nvim: Install conform.nvim

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "https://github.com/stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+                go = { "gopls" }
+            }
+        }
+    },
+}
