60c6cf897ff9e8fe1c0c2fd2d0016bd4baafea0e
Bez Hermoso <bezalelhermoso@gmail.com>
2024-04-21 20:16:45 -0700

    fix: attach cursorline appearance mods to an augroup

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -8,14 +8,18 @@ vim.api.nvim_create_autocmd('BufEnter', {
   end
 })
 
+local appearancegroup = vim.api.nvim_create_augroup('ModAppearance', { clear = false })
 -- Hide cursorline when buffer is not in focus [[[
-vim.api.nvim_create_autocmd({'VimEnter', 'WinEnter', 'BufWinEnter'}, { callback = function ()
+vim.api.nvim_create_autocmd({'VimEnter', 'WinEnter', 'BufWinEnter'}, {
+    group = appearancegroup,
+    callback = function ()
       vim.opt_local.cursorline = true
     end,
   }
 )
 
 vim.api.nvim_create_autocmd({'WinLeave'}, {
+    group = appearancegroup,
     callback = function ()
       vim.opt_local.cursorline = false
     end,
