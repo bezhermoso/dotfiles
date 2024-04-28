120367c8c6139a1dba42053746b0ce42be7fbaf3
Bez Hermoso <bezalelhermoso@gmail.com>
2024-04-27 22:41:36 -0700

    fix: nvim: nvim-tree: custom action in Telescope picker doesn't work well

--- .config/nvim/lua/plugins/nvim-tree/treeutils.lua
+++ .config/nvim/lua/plugins/nvim-tree/treeutils.lua
@@ -1,23 +1,22 @@
 -- Taken from https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#find-file-from-node-in-telescope
 --
 local api = require("nvim-tree.api")
-local openfile = require("nvim-tree.actions.node.open-file")
-local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
 local M = {}
 
-local view_selection = function(prompt_bufnr, map)
-  actions.select_default:replace(function()
-    actions.close(prompt_bufnr)
-    local selection = action_state.get_selected_entry()
-    local filename = selection.filename
-    if (filename == nil) then
-      filename = selection[1]
-    end
-    openfile.fn('preview', filename)
-  end)
-  return true
-end
+-- This cause an error: might be due to auto-closing nvim-tree config that I have in place.
+-- 
+-- local view_selection = function(prompt_bufnr, map)
+--   actions.select_default:replace(function()
+--     actions.close(prompt_bufnr)
+--     local selection = action_state.get_selected_entry()
+--     local filename = selection.filename
+--     if (filename == nil) then
+--       filename = selection[1]
+--     end
+--     openfile.fn('preview', filename)
+--   end)
+--   return true
+-- end
 
 function M.launch_live_grep(opts)
   return M.launch_telescope("live_grep", opts)
@@ -41,7 +40,8 @@ function M.launch_telescope(func_name, opts)
   opts = opts or {}
   opts.cwd = basedir
   opts.search_dirs = { basedir }
-  opts.attach_mappings = view_selection
+  -- Omitting this from the original article; Telescope's default action on select works just fine.
+  -- opts.attach_mappings = view_selection
   return require("telescope.builtin")[func_name](opts)
 end
 
