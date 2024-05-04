20dc7eb2b793cd921b22a45ef73350844a2ed118
Bez Hermoso <bezalelhermoso@gmail.com>
2024-05-03 22:36:45 -0700

    chore: Make Neo-tree reveal keymap silent

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -32,8 +32,8 @@ return {
             "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         },
         keys = {
-            { "<leader>n", ":Neotree reveal<CR>" },
-            { "\\", ":Neotree reveal<CR>" }
+            { "<leader>n", ":Neotree reveal<CR>", desc = "Neo-tree: reveal", silent = true },
+            { "\\", ":Neotree reveal<CR>", desc = "Neo-tree: reveal", silent = true }
         },
         config = function()
             require("neo-tree").setup({
