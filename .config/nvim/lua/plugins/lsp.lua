f737ae2b4a452b3ca2e42210487c5affeba9acf2
Bez Hermoso <bezalelhermoso@gmail.com>
2024-05-03 17:09:07 -0700

    fix: neovim: lsp: fix yamlls configuration + define custom tags e.g. "!secret ..."

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -131,23 +131,28 @@ return {
                     end,
                     yamlls = function()
                         require('lspconfig').yamlls.setup({
-                            schemas = {
-                                kubernetes = "*.yaml",
-                                ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-                                ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-                                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-                                ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-                                ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-                                ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-                                ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-                                ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-                                ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-                                ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] =
-                                "*api*.{yml,yaml}",
-                                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
-                                "*docker-compose*.{yml,yaml}",
-                                ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.son"] =
-                                    "*flow*.{yml,yaml}",
+                            settings = {
+                                yaml = {
+                                    schemas = {
+                                        ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+                                        ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                                        ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+                                        ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                                        ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                                        ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+                                        ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+                                        ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                                        ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+                                        ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+                                        ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.son"] = "*flow*.{yml,yaml}",
+                                        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+                                        kubernetes = "ccd-*/**/*.yaml.template",
+                                    },
+                                    customTags = {
+                                        "!secret sequence",
+                                        "!Secret sequence",
+                                    },
+                                },
                             },
                         })
                     end
