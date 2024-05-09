109e02b7abf20c2d5677517c21c95bfd35f16cf6
Bez Hermoso <bezalelhermoso@gmail.com>
2024-05-09 02:07:42 -0700

    feat: nvim: install neogit

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,35 +1,37 @@
+local git_installed = vim.fn.executable('git') == 1
+local gh_installed = vim.fn.executable('gh') == 1
 return {
     {
         "tpope/vim-fugitive",
-        enabled = vim.fn.executable('git') == 1,
+        enabled = git_installed,
         dependencies = {
             "tpope/vim-rhubarb",
         },
         cmd = { "Git", "Gdiffsplit", "Gvdiffsplit", "G", "GBrowse" },
-        keys = {
-            { "<leader>G", vim.cmd.G, desc = "Git status" }
-        },
+        -- keys = {
+        --     { "<leader>G", vim.cmd.G, desc = "Git status" }
+        -- },
     },
     {
         "lewis6991/gitsigns.nvim",
-        enabled = vim.fn.executable('git') == 1,
+        enabled = git_installed,
         keys = {
             { "<leader>gB", ":Gitsigns blame_line<CR>", {
                 desc = "Git: Show blame on current line",
-            }},
+            } },
             { "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {
                 desc = "Git: Toggle inline blames",
-            }},
+            } },
             { "<leader>gp", ":Gitsigns preview_hunk<CR>", {
                 desc = "Git: Preview hunk",
-            }},
+            } },
         },
         lazy = false,
         opts = {}
     },
     {
         "Almo7aya/openingh.nvim",
-        enabled = vim.fn.executable('gh') == 1,
+        enabled = gh_installed,
         lazy = false,
         config = function()
             require('openingh').setup()
@@ -46,14 +48,37 @@ return {
     },
     {
         "sindrets/diffview.nvim",
-        enabled = vim.fn.executable('git') == 1,
+        enabled = git_installed,
         event = "VeryLazy",
         opts = {},
     },
     {
         "pwntester/octo.nvim",
-        enabled = vim.fn.executable('gh') == 1,
+        enabled = gh_installed,
         event = "VeryLazy",
         opts = {},
-    }
+    },
+    {
+        "https://github.com/NeogitOrg/neogit",
+        enabled = git_installed,
+        dependencies = {
+            "nvim-lua/plenary.nvim",  -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+
+            -- Only one of these is needed, not both.
+            "nvim-telescope/telescope.nvim", -- optional
+            "ibhagwan/fzf-lua",              -- optional
+        },
+        keys = {
+            { "<leader>G", function() require('neogit').open({ kind = "floating" }) end, desc = "Neogit: Git status (floating)" },
+            { "<leader>gs", function() require('neogit').open({ kind = "split" }) end, desc = "Neogit: Git status (split)" }
+        },
+        config = {
+            graph_style = "unicode",
+            -- Use fzf sorter
+            telescope_sorter = function()
+                return require("telescope").extensions.fzf.native_fzf_sorter()
+            end,
+        },
+    },
 }
