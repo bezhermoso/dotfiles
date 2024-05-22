1bc65a2b738b668e6509abe3ea97ba8e3c8c14f0
Bez Hermoso <bezalelhermoso@gmail.com>
2024-05-21 20:27:59 -0700

    chore: nvim: remove nvim-tree

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -356,7 +356,7 @@ return {
         "https://github.com/antosha417/nvim-lsp-file-operations",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-tree.lua",
+            "nvim-neo-tree/neo-tree.nvim",
         },
         config = function()
             require("lsp-file-operations").setup()
