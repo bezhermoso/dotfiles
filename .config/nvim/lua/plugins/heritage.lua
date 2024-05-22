0bd5dd76915ecf8bf46849f3fda834b5325dbb19
Bez Hermoso <bezalelhermoso@gmail.com>
2024-05-21 20:28:32 -0700

    feat: nvim: install vim-heritage

---
+++ .config/nvim/lua/plugins/heritage.lua
@@ -0,0 +1,9 @@
+return {
+    "jessarcher/vim-heritage",
+    keys = {
+        -- Replaces the built-in gf mapping with a version that can handle non-existent files.
+        -- Together with vim-heritage, this allows you to open non-existent files even if they're in
+        -- a non-existing direectory. The plugin will create the directory heirarchy for you.
+        {"gf", ":edit <cfile><CR>", desc = "Edit file under cursor"}
+    }
+}
