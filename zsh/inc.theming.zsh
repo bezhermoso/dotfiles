af5f3835e8cbad92df72626969c74b5730751172
Bez Hermoso <bezalelhermoso@gmail.com>
2024-10-22 00:07:05 -0700

    feat: base16: install tinted-theming/tinted-jqp

--- zsh/inc.theming.zsh
+++ zsh/inc.theming.zsh
@@ -5,6 +5,13 @@ zi ice \
   atload"export BASE16_FZF_PATH=\$(pwd)"
 zi load tinted-theming/tinted-fzf
 
+# https://github.com/tinted-theming/tinted-jqp
+# atload: Sets the TINTED_FZF_PATH to the installation directory managed by zinit
+# This is used by tinted-shell's hook system to load reload the correct colors for FZF when the theme changes
+zi ice \
+  atload"export TINTED_JQP_PATH=\$(pwd)"
+zi load tinted-theming/tinted-jqp
+
 # Export the base16 color values to the shell as environment variables
 export BASE16_SHELL_ENABLE_VARS=1
 # atload: Sets the BASE16_SHELL_PATH to the installation directory managed by zinit, 
