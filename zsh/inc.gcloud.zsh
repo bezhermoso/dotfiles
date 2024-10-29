634cbca77a32f3e13dcf66201c6ce52f0dbe9b25
Bez Hermoso <bezalelhermoso@gmail.com>
2024-10-28 22:48:24 -0700

    feat: gcloud: use pyenv-managed 3.11.4 if installed

--- zsh/inc.gcloud.zsh
+++ zsh/inc.gcloud.zsh
@@ -2,6 +2,11 @@ if (( ! $+commands[brew] )); then
   return 0
 fi
 
+# If 3.11.4 is installed via pyenv, use it as the Python version for gcloud
+if [[ -x $HOME/.pyenv/versions/3.11.4/bin/python ]]; then
+  export CLOUDSDK_PYTHON=$HOME/.pyenv/versions/3.11.4/bin/python
+fi
+
 if [[ -n $HOMEBREW_PREFIX/share/google-cloud-sdk/*.zsh.inc(-.) ]]; then
   source $HOMEBREW_PREFIX/share/google-cloud-sdk/*.zsh.inc(-.)
 fi
