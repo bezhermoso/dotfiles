9c92ccc189a54f900b88517715ba16557fa35ae4
Bez Hermoso <bezalelhermoso@gmail.com>
2024-12-29 22:37:46 -0800

    feat: nvim: install tree-sitter-test

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -136,4 +136,15 @@ return {
             'nvim-treesitter/nvim-treesitter',
         },
     },
+    {
+        "tree-sitter-grammars/tree-sitter-test",
+        build = "mkdir parser && tree-sitter build -o parser/test.so",
+        ft = "test",
+        init = function()
+            -- toggle full-width rules for test separators
+            vim.g.tstest_fullwidth_rules = false
+            -- set the highlight group of the rules
+            vim.g.tstest_rule_hlgroup = "FoldColumn"
+        end
+    }
 }
