3d8ac00d7e4fa50c500755f66cabc815f196ede7
Bez Hermoso <bezalelhermoso@gmail.com>
2024-12-29 22:40:25 -0800

    feat: nvim: tree-sitter-ghostty: symlink highlights.scm from plugin folder

--- .config/nvim/after/queries/ghostty/highlights.scm
+++ .config/nvim/after/queries/ghostty/highlights.scm
@@ -3,26 +3,58 @@
 (comment) @comment
 
 ; Keys
-(property) @property
+(property) @variable
 
 ; Values
-(boolean_literal) @boolean
+(boolean) @boolean
 
 [
- (number_literal)
- (adjustment)
-] @number
-
-[
- (value)
- (raw_value)
- (string_literal)
+ (string)
+ (color)
 ] @string
 
 [
-    (hex_color)
-] @markup.bold
+ (number)
+ (adjustment)
+] @number
 
-(palette_value [(hex_color) (palette_index)] @markup.italic @markup.bold)
+; (color) are hex values
+(color "#" @punctuation.delimiter.special
+  (#eq? @punctuation.delimiter.special "#"))
 
-(config_file_directive (raw_value) @string.special.path)
+; `palette`
+(palette_index) @variable.member
+
+(palette_value "=" @operator
+  (#eq? @operator "="))
+
+; `config-file`
+(config_file_directive (property) @keyword.import)
+(config_file_directive (path_value) @string.special.path)
+
+; `keybind`
+(keybind_value) @string.special
+
+; clear is a special keyword that clear all existing keybind up to that point
+((keybind_value) @keyword
+                 (#eq? @keyword "clear"))
+
+(keybind_action) @function.call
+(action_argument) @variable.parameter
+
+[
+ "+"
+ "="
+ (keybind_trigger ">")
+] @operator
+
+; NOTE: The order here matters!
+[
+ (modifier_key)
+ (key)
+] @constant.builtin
+
+[
+ (key_qualifier)
+ (keybind_modifier)
+] @attribute
