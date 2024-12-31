3e27688521ee5ab74ed0f0631a16beabf130ff1d
Bez Hermoso <bezalelhermoso@gmail.com>
2024-12-30 22:45:17 -0800

    feat: remove tinted-theming/* zsh hooks: they are replaced by tinty

--- zsh/inc.theming.zsh
+++ zsh/inc.theming.zsh
@@ -1,32 +1,3 @@
-# https://github.com/tinted-theming/tinted-fzf
-# atload: Sets the BASE16_FZF_PATH to the installation directory managed by zinit
-# This is used by tinted-shell's hook system to load reload the correct colors for FZF when the theme changes
-# zi ice \
-#   atload"export BASE16_FZF_PATH=\$(pwd)"
-# zi load tinted-theming/tinted-fzf
-
-# https://github.com/tinted-theming/tinted-jqp
-# atload: Sets the TINTED_JQP_PATH to the installation directory managed by zinit
-# This is used by tinted-shell's hook system to load reload the correct colors for FZF when the theme changes
-# zi ice \
-#   atload"export TINTED_JQP_PATH=\$(pwd)"
-# zi load tinted-theming/tinted-jqp
-
-# Export the base16 color values to the shell as environment variables
-export BASE16_SHELL_ENABLE_VARS=1
-# atload: Sets the BASE16_SHELL_PATH to the installation directory managed by zinit, 
-# and sets the theme to gruvbox-material-dark-hard if it is not already set.
-# src: Loads the profile helper script that actually sets the colors in the shell
-# if: We don't need this when using Ghostty
-zi ice \
-  atload'export BASE16_SHELL_PATH=$(pwd); [[ -z $BASE16_THEME ]] && base16_gruvbox-material-dark-hard' \
-  if'[[ "$__CFBundleIdentifier" != *ghostty ]]' \
-  src"profile_helper.sh"
-zi load tinted-theming/tinted-shell
-
-# # base16 theme picker
-# zi load bezhermoso/zsh-choose-base16
-
 zi ice atpull"%atclone" atclone"_fix-omz-plugin"
 zi snippet OMZP::colored-man-pages
 less_termcap[so]="${fg[black]}${bg[yellow]}"
