1cfee8e7f43efc2dbe155fe48d5db991ef1d583a
Bez Hermoso <bezalelhermoso@gmail.com>
2025-05-01 18:56:29 -0700

    chore: disable some lua diagnostics on certain lines

--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
         build = ":TSUpdate",
         config = function()
             local configs = require("nvim-treesitter.configs")
+            ---@diagnostic disable-next-line: missing-fields
             configs.setup({
                 ensure_installed = {
                     "bash",
