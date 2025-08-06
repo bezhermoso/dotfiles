57155c9ac47839b3d880cd4a38e9469dc1d5f66b
Bez Hermoso <bezalelhermoso@gmail.com>
2025-08-06 00:09:41 -0700

    style: spaces

--- dot-config/nvim/lua/plugins/ufo.lua
+++ dot-config/nvim/lua/plugins/ufo.lua
@@ -1,4 +1,3 @@
-
 -- Apparently there are filetypes whose LSP does not support folding. We can fallback to Treesitter instead.
 -- Source: https://github.com/chrisgrieser/.config/blob/a3c5be040b46f7dc43e5e10c15c807fa505bcb10/nvim/lua/plugins/folding-plugins.lua#L65-L72
 local treesitter_based_folding_filetypes = {
@@ -17,8 +16,8 @@ return {
         { "nvim-treesitter/nvim-treesitter" },
     },
     keys = {
-        { "zR", function() require('ufo').openAllFolds() end, desc = "Folds: open all" },
-        { "zM", function() require('ufo').closeAllFolds() end, desc = "Folds: close all" },
+        { "zR", function() require('ufo').openAllFolds() end,           desc = "Folds: open all" },
+        { "zM", function() require('ufo').closeAllFolds() end,          desc = "Folds: close all" },
         { "K",  function() require("core.ufo").peek_or_lsp_hover() end, desc = "LSP/Folding: LSP lookup or preview folded range" },
     },
     init = function()
@@ -30,13 +29,13 @@ return {
     config = function()
         require("ufo").setup({
             close_fold_kinds_for_ft = {
-                php = {"comment", "imports"},
+                php = { "comment", "imports" },
             },
-            provider_selector = function (_, ftype, _)
+            provider_selector = function(_, ftype, _)
                 if vim.tbl_contains(treesitter_based_folding_filetypes, ftype) then
-                    return {"treesitter", "indent"}
+                    return { "treesitter", "indent" }
                 end
-                return {"lsp", "indent"}
+                return { "lsp", "indent" }
             end
         })
     end,
