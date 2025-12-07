cca85b62a0667a3cb4b2c4913d7fd2dcad036f94
Bez Hermoso <bezalelhermoso@gmail.com>
2025-12-06 19:40:53 -0800

    feat: zsh: use Homebrew's command not found handler

--- zsh/inc.macos.zsh
+++ zsh/inc.macos.zsh
@@ -6,3 +6,14 @@ fi
 zi ice atpull"%atclone" atclone"_fix-omz-plugin"
 zi snippet OMZP::macos
 
+# Command not found handler with Homebrew integration
+# Suggests which formula to install when a command is not found
+# Native in Homebrew since Sept 2025 (no tap required)
+if (( $+commands[brew] )); then
+  hb_cnf_handler="$(brew --repository)/Library/Homebrew/cmd/command-not-found-init.rb"
+  if [[ -f "$hb_cnf_handler" ]]; then
+    eval "$(brew command-not-found-init)"
+  fi
+  unset hb_cnf_handler
+fi
+
