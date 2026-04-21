c66063850cc54160953b60aca7186a46794745d0
Bez Hermoso <bezalelhermoso@gmail.com>
2026-04-20 22:11:49 -0700

    feat: nvim: markdown plugins

--- dot-config/nvim/lua/plugins/markdown.lua
+++ dot-config/nvim/lua/plugins/markdown.lua
@@ -8,4 +8,25 @@ return {
         },
         ft = { "markdown", "Avante" },
     },
+    {
+        "selimacerbas/markdown-preview.nvim",
+        dependencies = { "selimacerbas/live-server.nvim" },
+        config = function()
+            require("markdown_preview").setup({
+                -- all optional; sane defaults shown
+                port = 8421,
+                open_browser = true,
+                debounce_ms = 300,
+            })
+        end,
+    },
+    {
+        "roodolv/markdown-toggle.nvim",
+        config = function()
+            require("markdown-toggle").setup({
+                use_default_keymaps = true
+            })
+        end,
+    }
+
 }
